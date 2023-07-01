d88c9bcc20f3ea99c3fc513ef32798b8680c6eb7
haramishra <haraprasad45@gmail.com>
2023-07-02 04:08:04 +0530

    hero section bg added

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,8 @@ import { allBlogs } from 'contentlayer/generated'
 import type { Blog } from 'contentlayer/generated'
 import Landing from '@/components/landing/Index'
 import landginData from '@/data/landingData'
+import styles from './index.module.css'
+import clsx from 'clsx'
 
 const MAX_DISPLAY = 5
 
@@ -23,6 +25,22 @@ export const getStaticProps = async () => {
 export default function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
+      <div className="px-4 sm:px-6 md:px-8">
+        <div
+          className={clsx(
+            'absolute inset-0 bottom-10 -z-30 h-screen bg-slate-50 bg-bottom bg-no-repeat dark:bg-[#0B1120]',
+            styles.beams
+          )}
+        >
+          {/* <div
+            className="bg-grid-slate-900/[0.04] dark:bg-grid-slate-400/[0.05] absolute inset-0 bg-[bottom_1px_center] dark:border-b dark:border-slate-100/5 dark:bg-bottom"
+            style={{
+              maskImage: 'linear-gradient(to bottom, transparent, black)',
+              WebkitMaskImage: 'linear-gradient(to bottom, transparent, black)',
+            }}
+          ></div> */}
+        </div>
+      </div>
       <Landing data={landginData} />
     </>
   )
